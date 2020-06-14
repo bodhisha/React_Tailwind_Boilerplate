71139ab6bff3aebf100abb28a4296d899c1dd823
bodhisha <bodhisha@gmail.com>
2020-06-15 02:31:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen items-center justify-center">
+      <div className="bg-white my px-8 py-6 center max-w-md my-60 rounded-lg shadow-lg">
+        <div className="flex justify-center items-center">
+            <div className="px-2 py-1 bg-gray-600 text-xl text-green-100 rounded">React + Tailwind</div>
+        </div>
+        <div className="mt-4">
+            <div className="text-lg text-gray-700 font-medium" >A boilerplate code for React js with tailwindcss configured</div>
+        </div>
     </div>
+    </div>
+
+    
   );
 }
 
